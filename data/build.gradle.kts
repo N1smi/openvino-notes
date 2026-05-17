bdd03be0f02ed7384e25323f1bbe6ea5ca9708d0
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2026-05-17 16:20:52 +0300

    feat: add synchronization for all media New methods in MediaDao: getAllMedia(), getUnsyncedMedia(), update() New method in FirebaseCloudDataSource: listMediaMetadata() Add CloudMediaMetadata in CloudDataSource Change PushChanges and PullUpdates for sync all media

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -99,4 +99,5 @@ dependencies {
     testImplementation(libs.androidx.work.testing)
     testImplementation(libs.koin.test)
     testImplementation(libs.koin.test.junit4)
+    testImplementation(libs.mockk.agent.jvm)
 }
